394f75194c675f9394eaab8f4b260ad288f3d583
ryanvanderstelt <ryan@vstelt.com>
2023-12-31 20:24:45 -0700

    haha place thine enemies death

--- src/main.py
+++ src/main.py
@@ -15,6 +15,16 @@ def click_play(engine):
     engine.ui = game_screen
     engine.paused = False
 
+def click_grid(engine):
+    if engine.unit_bought is not None and engine.entity_map[engine.mouse_pos[1] // 40][engine.mouse_pos[0] // 40] is None:
+
+        id = engine.ecs.new_entity()
+        engine.ecs.positions[id] = pygame.Rect(engine.mouse_pos[0] - engine.mouse_pos[0] % 40, engine.mouse_pos[1] - engine.mouse_pos[1] % 40, 40.0, 40.0)
+        engine.ecs.colors[id] = (255, 0, 0)
+        
+        engine.entity_map[engine.mouse_pos[1] // 40][engine.mouse_pos[0] // 40] = id
+        engine.unit_bought = None
+    
 
 title_screen = [
     (pygame.Rect(0, 0, 200, 20), (255, 0, 0), "GameNameHere", None),
@@ -25,7 +35,10 @@ pause_screen = [
     (pygame.Rect(400, 400, 200, 20), (0, 255, 0), "RESUME", click_play)
 ]
 
-game_screen = []
+game_screen = [
+    (pygame.Rect(0, 0, 800, 520), None, None, click_grid),
+    (pygame.Rect(0, 520, 800, 80), (0, 255, 0), "You have no money :_(", None)
+]
 
 
 victory_screen = []
@@ -37,6 +50,8 @@ class GameEngine:
     mouse_pos = None
     running = True
     paused = True
+    unit_bought = None
+    entity_map = [[None for _ in range(20)] for _ in range(13)]
 
     ui = title_screen
 
@@ -59,6 +74,7 @@ class GameEngine:
         # self.add_conveyor(EntityType.CONV_LEFT, 100, 100)
         # self.add_conveyor(EntityType.CONV_UP, 80, 100)
 
+
         # creature
         # id = self.ecs.new_entity()
         # self.ecs.types[id] = EntityType.CREATURE
@@ -67,6 +83,7 @@ class GameEngine:
         # self.ecs.colors[id] = (0, 0, 255)
         # self.ecs.velocities[id] = pygame.math.Vector2(0, 0)
         # self.ecs.collidable[id] = True
+
         
         # Load font(s)
         self.font = pygame.freetype.Font('./assets/russo.ttf', 24)
@@ -123,11 +140,14 @@ class GameEngine:
 
         for i in range(nr+1):
             y = i*w
+            pygame.draw.line(self.screen, (255, 255, 255), (0, y-1), (800, y-1))
             pygame.draw.line(self.screen, (255, 255, 255), (0, y), (800, y))
 
         for i in range(nc+1):
             x = i*w
+            pygame.draw.line(self.screen, (255, 255, 255), (x-1, 0), (x-1, 600))
             pygame.draw.line(self.screen, (255, 255, 255), (x, 0), (x, 600))
+        
         
  
     def draw(self):
@@ -150,6 +170,9 @@ class GameEngine:
     # Draw the ui of the screen
     def draw_ui(self):
         for (rect, color, text, _) in self.ui:
+            if color is None:
+                continue
+
             pygame.draw.rect(self.screen, color, rect)
             self.draw_text(rect.x, rect.y, text, (0, 0, 0))
         
