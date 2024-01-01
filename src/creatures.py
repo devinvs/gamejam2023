6c97901ef95588d8db2e0cd989459766adb347e7
Devin Vander Stelt <devin@vstelt.dev>
2023-12-31 21:50:44 -0700

    add spawn system

---
+++ src/creatures.py
@@ -0,0 +1,23 @@
+import time
+import random
+
+delay = 0
+last_spawn = 0
+
+def creature_system(ecs):
+    global delay
+    global last_spawn
+
+    if time.monotonic() - last_spawn - delay <= 0.0:
+        return
+
+    print("letsago")
+
+    # Spawn
+    health = random.randrange(5, 20)
+    ecs.add_creature(50, 50, health)
+
+    # Set delay
+    delay = random.uniform(1.0, 6.0)
+    last_spawn = time.monotonic()
+    
